4a160d9fb2d1b236ef3da3fd32e8924b1246c714
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-07 20:18:22 +0100

    Up to end of Step 11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,23 +1,44 @@
-class Oystercard
-  MAXIMUM_BALANCE = 90
-  attr_reader :balance
+class OysterCard
+  MAXIMUM_LIMIT = 90
+  MINIMUM_BALANCE = 1
+  MIN_FARE = 1
+  attr_accessor :balance, :entry_station
 
-  def initialize(balance = 0)
-    @balance = balance
+  def initialize(entry_station = nil)
+    @balance = 0
+    @entry_station = entry_station
   end
 
   def top_up(amount)
-    raise "Exceeded maximum amount: £#{MAXIMUM_BALANCE}" if over_max_balance?(amount)
+    fail "Max limit of #{MAXIMUM_LIMIT}, cannot top-up" if max_limit?(amount)
     @balance += amount
+  end
+
+  def in_journey?
+    !@entry_station.nil?
+  end 
+
+  def touch_in(station)
+    raise 'Insufficient funds, please top up' if minimum_balance?
+    @entry_station = station
+  end 
+
+  def touch_out
+    deduct(MIN_FARE)
+    @entry_station = nil
+  end 
+
+  private
+
+  def max_limit?(amount)
+    @balance + amount > MAXIMUM_LIMIT
+  end
+
+  def minimum_balance?
+    @balance < MINIMUM_BALANCE
   end
 
   def deduct(amount)
     @balance -= amount
   end
-
-  private
-
-  def over_max_balance?(amount)
-    (amount + balance) > MAXIMUM_BALANCE
-  end
-end 
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,24 +1,68 @@
 require 'oystercard'
-describe Oystercard do
-  oystercard = Oystercard.new
-  it { expect(oystercard).to be_an_instance_of(Oystercard) }
 
-  it { expect(subject.balance).to eq (0) }
+describe OysterCard do
+  let(:station) { double :station }
 
-  describe '#top_up' do
-    it 'adds to the balance value' do
-      expect { subject.top_up(15) }.to change { subject.balance }.by(15)
+  context '#balance' do
+    it 'creates new card with 0 balance' do
+      expect(subject.balance).to eq(0)
     end
-    it 'throws an error when balance exceed 90' do
-      max_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(max_balance)
-      expect{ subject.top_up 1 }.to raise_error "Exceeded maximum amount: £#{max_balance}"
+  end
+
+  context '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up the balance' do
+      expect { subject.top_up(50) }.to change { subject.balance }.by(50)
+    end
+
+    it 'raises an error when max_limit reached' do
+      maximum_limit = OysterCard::MAXIMUM_LIMIT
+      subject.top_up(maximum_limit)
+      expect { subject.top_up 1 }.to raise_error "Max limit of #{maximum_limit}, cannot top-up"
+    end
+  end
+
+  context '#in_journey?' do
+    it 'starts off nil before touch in' do
+      expect(subject.in_journey?).to eq(false)
+    end
+  end 
+  
+  context '#touch_in' do
+    it { is_expected.to respond_to(:touch_in).with(1).argument }
+
+    it 'changes the value of in_journey to true' do
+      allow(subject).to receive(:minimum_balance?).and_return false
+      subject.touch_in(station)
+      expect(subject.in_journey?).to be true
+    end 
+
+    it 'raises an error if balance is less than 1' do
+      expect { subject.touch_in(station) }.to raise_error('Insufficient funds, please top up')
+    end
+
+    it 'stores the value of entry_station' do
+      allow(subject).to receive(:minimum_balance?).and_return false
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq(station)
     end
   end 
 
-  describe '#deduct' do
-    it 'deducts from the balance value' do
-      expect { subject.deduct(15) }.to change { subject.balance }.by(-15)
+  context '#touch_out' do
+    it { is_expected.to respond_to(:touch_out) }
+
+    it 'changes the value of in_journey back to false' do
+      allow(subject).to receive(:minimum_balance?).and_return false
+      subject.touch_in(station)
+      subject.touch_out
+      expect(subject.in_journey?).to be false
+    end 
+
+    it 'deducts the minimum charge from the card' do
+      allow(subject).to receive(:minimum_balance?).and_return false
+      subject.touch_in(station)
+      expect { subject.touch_out }.to change { subject.balance }.by(-OysterCard::MIN_FARE)
     end
   end
-end 
+end
